1fc44a8f3b5f529d1706b8a8e4396ab712ef453b
Tao <uku.ele@gmail.com>
2016-11-21 11:22:01 -0500

    Add files via upload

---
+++ dotismember.m
@@ -0,0 +1,9 @@
+function [ bo ] = dotismember( dot, celli,unitnm, vb )
+%DOTISMEMBER Summary of this function goes here
+%   Detailed explanation goes here
+for i=1:unitnm,
+    n=cell2unit(celli,i,unitnm);
+    
+
+end
+
